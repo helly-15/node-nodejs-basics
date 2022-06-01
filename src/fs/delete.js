8e115bc3adefbfb69f08c4cad8b2b273809c0364
Lenochka <lvutina.en@gmail.com>
2022-06-01 22:24:27 +0300

    task 4 about delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.join(__dirname, 'files',"fileToRemove.txt");
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed')
+    }
+    fs.unlink(filePath, (err) => {
+        if (err) throw err;
+        console.log('path/file.txt was deleted');
+    });
 };
+remove();
