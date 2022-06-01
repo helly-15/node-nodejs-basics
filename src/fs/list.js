24f9d52a630dd15c08923e789944ca18e880db16
Lenochka <lvutina.en@gmail.com>
2022-06-01 22:29:13 +0300

    task 5 about list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+    let sourceFolder = path.join(__dirname, 'files');
+    // Write your code here
+    if (!fs.existsSync(sourceFolder)) {
+        throw new Error('FS operation failed')
+    }
+
+    let files = fs.readdirSync(sourceFolder);
+    console.log(files)
 };
+list();
