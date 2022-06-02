1d6438875c478ca076d93ba53ebb6c25cf06a228
Lenochka <lvutina.en@gmail.com>
2022-06-02 14:20:17 +0300

    task 8 about argv parsing

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
+//--propName value --prop2Name value2
 export const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    process.argv.forEach((arg,index)=>{
+        if (index%2===0 && index !==0){
+            console.log(`${arg.substring(2)} is ${process.argv[index+1]}`)
+        }
+    })
 };
+parseArgs();
