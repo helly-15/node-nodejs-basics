7e715d6ffb4aadb6a7a1a11137e4774464611f69
Lenochka <lvutina.en@gmail.com>
2022-06-03 16:25:28 +0300

    task 13 about transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import {Transform} from 'stream'
+
 export const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    let fileStream = process.stdin;
+    const transformedData= process.stdout;
+
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.reverse());
+        },
+    });
+
+    fileStream.pipe(reverse).pipe(transformedData);
 };
+transform();
