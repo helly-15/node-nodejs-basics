e6e4f9e67e9c997b9397f93ec9dedf253e577d54
Lenochka <lvutina.en@gmail.com>
2022-06-03 21:38:04 +0300

    task 15 about gzip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
+import {createReadStream, createWriteStream} from "fs";
+import path, {dirname} from "path";
+import {pipeline} from "stream";
+import {fileURLToPath} from "url";
+import * as zlib from "zlib";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const gzip = zlib.createGunzip();
+    const source = createReadStream('archive.gz');
+    const destination = createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
+decompress();
