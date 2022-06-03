c896cc182ff38a408cd8ab7e8c87f40cbd0d1e36
Lenochka <lvutina.en@gmail.com>
2022-06-03 15:53:46 +0300

    task 12 about writeable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+// Enter any texts in console( User input)
+import fs from 'fs';
+
+import {fileURLToPath} from "url";
+import path, {dirname} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 export const write = async () => {
-    // Write your code here 
+    // Write your code here
+    let writebleProcess = process.stdin;
+    let writeStream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'))
+    writebleProcess.pipe(writeStream)
 };
+write();
