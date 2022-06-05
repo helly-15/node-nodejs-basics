b3fe8df2e643dc5bb7665a359a2fcef9e12fcd0d
Lenochka <lvutina.en@gmail.com>
2022-06-05 15:50:38 +0300

    task 16 about workers

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,23 @@
+import {Worker, isMainThread, parentPort, workerData} from 'worker_threads';
+import {fileURLToPath} from "url";
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+const __filename = fileURLToPath(import.meta.url);
+
 export const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    if (isMainThread) {
+        let x = 6;
+        const worker = new Worker(__filename,{ workerData: {x} });
+        worker.on('message', (data) => {
+            console.log(data)
+        });
+    } else {
+        let {x} = workerData
+        let calcRes = nthFibonacci(x)
+        parentPort.postMessage(calcRes)
+    }
 };
+sendResult()
